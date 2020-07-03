cd6c2c9a71e6ea203eaa6c4b135bd62ee6d7266b
Milad Vafaeifard <milad_vafaeifard@rcomext.com>
2020-07-03 15:15:40 +0200

    Init files

---
+++ src/WhereAmI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Device.Location;
+
+namespace WhereAmI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Starting GeoCoordinate Watcher...");
+
+            // 2. Use the GeoCoordinate Watcher
+            var watcher = new GeoCoordinateWatcher();
+
+            watcher.StatusChanged += (sender, ev) =>
+            {
+                Console.WriteLine($"GeoCoordinateWatcher:StatusChanges:{ev.Status}");
+            };
+
+
+            watcher.PositionChanged += (sender, ev) =>
+            {
+                Console.WriteLine($"GeoCoordinateWatcher:PositionChanged:{ev.Position.Location}");
+                
+                MapImage.Show(ev.Position.Location);
+            };
+
+            watcher.MovementThreshold = 100;
+
+            watcher.Start();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
